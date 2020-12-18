93de1f04bbba95942ecdccf929f06f5491ef16b4
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 16:05:30 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,14 @@
+var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+
+// Add your functions and code here
+function destructivelyAppendKitten(){
+  return kittens.push("Ralph")
+}
+
+function destructivelyPrependKitten(){
+  return kittens.unshift("Bob")
+}
+
+function destructivelyRemoveLastKitten(){
+  return kittens.pop()
+}
