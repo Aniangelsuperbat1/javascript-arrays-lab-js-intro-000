2bd06fcd48d21a8cd9d7b8ddde845d89024afa7d
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 15:56:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-var kittens = ["Milo", "Otis", "Garfield"] //define your array here
+var kittens = [] //define your array here
 
 // Add your functions and code here
-function destructivelyAppendKitten(){
-  return appendKitten()
-}
