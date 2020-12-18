e5da2618636c6fe83475f1363ce3e7352178468d
Patrick Zhen <zhen.patrick@gmail.com>
2020-12-18 16:04:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ var kittens = ["Milo", "Otis", "Garfield"] //define your array here
 function destructivelyAppendKitten(){
   return kittens.push("Ralph")
 }
+
+function destructivelyPrependKitten(){
+  return kittens.unshift("Ralph")
+}
